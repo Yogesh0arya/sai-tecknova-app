e9777b909d4fa4cadc2d87a694223c2c18bf9127
Dipesh <dipesharya28@gmail.com>
2022-01-22 00:25:03 +0530

    second

--- components/About.js
+++ components/About.js
@@ -14,7 +14,7 @@ function About() {
             <div className="max-w-6xl mt-24 xl:mx-auto mx-5">
                 <h1 className="text-4xl font-bold text-center my-5 text-black">About</h1>
                 <p data-aos="fade-up" className="text-center text-gray-700">
-                    Greetings from SAI - TECKNOVA, a software skill based training and development centre for IT aspirants. Today's demand for job in IT is highly competitive due to change in IT market. IT industry expect students to be trained in field of Data Science / Analytics, Machine Learning, AI and related domains. The need for bridging the gap between the industrial demand and Universities academic curriculum is on priority. The need to make our students  to be industry ready with consistent support on improving their quality is on priority. 
+                    Greetings from SAI - TECKNOVA, a software skill based training and development centre for IT aspirants. Today&apos;s demand for job in IT is highly competitive due to change in IT market. IT industry expect students to be trained in field of Data Science / Analytics, Machine Learning, AI and related domains. The need for bridging the gap between the industrial demand and Universities academic curriculum is on priority. The need to make our students  to be industry ready with consistent support on improving their quality is on priority. 
     SAI TECKNOVA offers the following skill based courses through online mode to students.
                 </p>
 
